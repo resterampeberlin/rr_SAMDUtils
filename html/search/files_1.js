80385353ba15b165303a01183d8fbaa685d0fa3d
Markus Nickels <MarkusNickels@mac.com>
2022-02-20 16:54:03 +0100

    Documentation updated

---
+++ html/search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['rr_5fsamdutils_2ecpp_0',['rr_SAMDUtils.cpp',['../rr___s_a_m_d_utils_8cpp.html',1,'']]],
+  ['rr_5fsamdutils_2eh_1',['rr_SAMDUtils.h',['../rr___s_a_m_d_utils_8h.html',1,'']]]
+];
